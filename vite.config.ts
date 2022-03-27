a0f87d145b55ede5d9add2e8e7130d3a65424406
Malcolm Kee <malcolm.keeweesiong@gmail.com>
2022-03-27 17:55:51 +0800

    feat: add required icons and manifest settings

--- vite.config.ts
+++ vite.config.ts
@@ -13,11 +13,38 @@ export default defineConfig({
   plugins: [
     react(),
     VitePWA({
+      includeAssets: [
+        'favicon.svg',
+        'favicon.ico',
+        'robots.txt',
+        'apple-touch-icon.png',
+      ],
       strategies: 'injectManifest',
       srcDir: 'src',
       filename: 'sw.js',
-      devOptions: {
-        enabled: true,
+      manifest: {
+        name: 'Personal Expense Tracking',
+        short_name: 'Expense',
+        description: 'Personal Expense Tracking App saved in Airtable',
+        theme_color: '#ec4899',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
       },
     }),
   ],
